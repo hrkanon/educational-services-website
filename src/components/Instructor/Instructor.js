b57f4d7b7f2cc2699d599421aef54a20bf8be99f
hrkanon <habibur3753@gmail.com>
2021-10-05 14:12:27 +0600

    instructor code

--- src/components/Instructor/Instructor.js
+++ src/components/Instructor/Instructor.js
@@ -4,19 +4,21 @@ import "./Instructor.css";
 const Instructor = (props) => {
   const { instructor, image, rating, reviews, profession } = props.instructor;
   return (
-    <div className=" col instructor-card border">
-      <div className="d-flex align-items-center me-2">
-        <div className="text-center">
-          <img className="rounded-pill w-75" src={image} alt="" />
-        </div>
-        <div>
-          <h5 className="card-title">{instructor}</h5>
-          <div className="card-body">
-            <small>{profession}</small>
-            <p>
-              <i className="fas fa-trophy"></i> {rating} stars <br />{" "}
-              <i className="fas fa-users"></i> {reviews} reviews
-            </p>
+    <div>
+      <div className="col shadow p-2 bg-body instructor-card">
+        <div className="d-flex align-items-center p-3">
+          <div className="instructor-img">
+            <img className="rounded-pill" src={image} alt="" />
+          </div>
+          <div className="ps-2">
+            <h5 className="fw-bold">{instructor}</h5>
+            <div>
+              <small>{profession}</small>
+              <p>
+                <i className="fas fa-trophy"></i> {rating} stars <br />{" "}
+                <i className="fas fa-users"></i> {reviews} reviews
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Instructors/Instructors.js
+++ src/components/Instructors/Instructors.js
@@ -10,16 +10,11 @@ const Instructors = () => {
       .then((data) => setInstructors(data));
   }, []);
   return (
-    <div>
-      <div className="container my-2 instructors-container">
-        <div className="row row-cols-1 row-cols-md-3">
-          {instructors.map((instructor) => (
-            <Instructor
-              key={instructor.id}
-              instructor={instructor}
-            ></Instructor>
-          ))}
-        </div>
+    <div className="container">
+      <div className="row row-cols-1 row-cols-md-3 g-2 my-2">
+        {instructors.map((instructor) => (
+          <Instructor key={instructor.id} instructor={instructor}></Instructor>
+        ))}
       </div>
     </div>
   );
